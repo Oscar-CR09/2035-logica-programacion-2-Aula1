447804e6e3db3b46991bc83ce7eb1ae6a771e1d6
Oscar <oscarcr189@outlook.com>
2024-01-23 21:26:47 -0600

    doom

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 let titulo = document.querySelector('h1');
 titulo.innerHTML='juego del numero secreto';
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML='Indica un numero del 1 al 10';
+
