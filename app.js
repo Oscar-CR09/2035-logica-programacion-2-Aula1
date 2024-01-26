44d7831e1cea4d7cc44b0f143995e83becd0eb67
Oscar <oscarcr189@outlook.com>
2024-01-25 21:38:46 -0600

    typos de datos

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
+let numeroSecreto = generarNumeroSecreto();
+
 
 function asignarTextoElemento(elemento, texto){
 
     let elementoHtml = document.querySelector(elemento);
     elementoHtml.innerHTML=texto;
+    return;
 
 }
-function intentoDeUsuario(){
-    alert('Click desde el boton');
+function verificarIntento(){
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    console.log(typeof(numeroDeUsuario));
+    console.log(numeroSecreto);
+    console.log(typeof(numeroSecreto));
+    console.log(numeroDeUsuario);
+    console.log(numeroDeUsuario === numeroSecreto);
+    return;
 }
 
+function generarNumeroSecreto() {
+    return Math.floor(Math.random()*10)+1;
+
+    
+}
 
 asignarTextoElemento('h1','Juego del número secreto!');
 asignarTextoElemento('p','Indica un número del 1 al 10');
