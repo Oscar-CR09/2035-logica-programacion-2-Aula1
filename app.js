78be1f63736dd370622dfe1e9517fe7683215cb0
Oscar <oscarcr189@outlook.com>
2024-01-28 21:35:48 -0600

    iniciar juego falta desabilitar boton

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let numeroSecreto = generarNumeroSecreto();
-let intentos = 1;
+let numeroSecreto = 0;
+let intentos = 0;
 
 
 function asignarTextoElemento(elemento, texto){
@@ -16,24 +16,49 @@ function verificarIntento(){
 
     if (numeroDeUsuario === numeroSecreto) {
         asignarTextoElemento(`p`,`acertaste el numero ${intentos} ${(intentos===1?'vez':'veces')}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
     }else {
+        //el usuario no acerto 
+
         if (numeroDeUsuario>numeroSecreto) {
             asignarTextoElemento('p','El numero secreto es menor');
         }else{
             asignarTextoElemento('p','El numero secreto es mayor');
         }
         intentos++;
+        limpiarCaja();
     }
  
     return;
 
 }
 
+function limpiarCaja() {
+    document.querySelector('#valorUsuario').value='';
+    
+}
 function generarNumeroSecreto() {
     return Math.floor(Math.random()*10)+1;
 
     
 }
+function condicionesIniciales() {
+    asignarTextoElemento('h1','Juego del número secreto!');
+    asignarTextoElemento('p','Indica un número del 1 al 10');
+    numeroSecreto = generarNumeroSecreto();
+    intentos=1;
+}
 
-asignarTextoElemento('h1','Juego del número secreto!');
-asignarTextoElemento('p','Indica un número del 1 al 10');
+function reiniciarJuego() {
+    //limpiar la caja, 
+    limpiarCaja();
+    //indicar el mensaje de intervalo de numeros,
+    // generar el numero aleatorio, 
+    // iniciar intentos 
+    condicionesIniciales();
+    //desabilitar el boton de nuevo juego
+
+ 
+}
+
+condicionesIniciales();
